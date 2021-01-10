7420ed3cfc68019b3d51cca38839974b686c3a6b
iamkanishka <kanishkabc123@gmail.com>
2021-01-10 20:21:11 +0530

    made some changes

--- lib/webservice.dart
+++ lib/webservice.dart
@@ -6,28 +6,24 @@ import 'package:http/http.dart' as http;
 import 'package:http/http.dart';
 
 class Webservice {
-  
   Future<Response> _getStory(int storyId) {
     return http.get(UrlHelper.urlForStory(storyId));
   }
 
   Future<List<Response>> getCommentsByStory(Story story) async {
-
-    return Future.wait(story.commentIds.map((commentId)  { 
-        return http.get(UrlHelper.urlForCommentById(commentId));
+    return Future.wait(story.commentIds.map((commentId) {
+      return http.get(UrlHelper.urlForCommentById(commentId));
     }));
-
   }
 
   Future<List<Response>> getTopStories() async {
-    
     final response = await http.get(UrlHelper.urlForTopStories());
     if (response.statusCode == 200) {
       Iterable storyIds = jsonDecode(response.body);
-      return Future.wait(storyIds.take(10).map((storyId) {
+      return Future.wait(storyIds.take(15).map((storyId) {
+        print(storyIds);
         return _getStory(storyId);
       }));
-
     } else {
       throw Exception("Unable to fetch data!");
     }
